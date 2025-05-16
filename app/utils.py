251e60684f25ff8b8c205250062caaad3b63bd23
ruslan <rzaevr133@gmail.com>
2025-05-16 16:11:11 +0300

    В С Е(версия без токена)

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,5 @@
 import json
 import httpx
-import os
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from fastapi import Depends, HTTPException, status
 from dotenv import load_dotenv
 from sqlalchemy.orm import class_mapper
 
@@ -21,20 +18,7 @@ def result_to_json(result):
     return json.loads(json.dumps(result_list, ensure_ascii=False))
 
 
-security = HTTPBearer()
-api_token = os.getenv('TOKEN')
-
-
 async def post_api(path: str, json):
     async with httpx.AsyncClient() as client:
         return (await client.post(f'http://127.0.0.1:8000/{path}', json=json,
-                                  headers={"Authorization": f"Bearer {api_token}"})).json()
-
-
-def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
-    token = credentials.credentials
-    if token != api_token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or missing token",
-        )
+                                  )).json()
